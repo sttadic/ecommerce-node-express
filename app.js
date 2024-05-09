e3f02267a49ddbe18c8371e3383bc292f4f444df
sttadic <stjepan.tadich@gmail.com>
2024-05-09 16:38:50 +0100

    Initial commit. Imports, connection to database, view engine all configured in app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+// Import and create a new instance of express
+const express = require("express");
+const app = express();
+
+// Import bodyparser and set urlencoded parser as middleware for the application with extended parsing mode
+const bodyParser = require("bodyparser");
+app.use(bodyParser.urlencoded({extend: true}));
+
+// Set up the view engine
+app.set("view engine", "ejs");
+
+// Import mysql2 module and create a connection to the database
+const mysql = require("mysql2");
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: ""
+});
+connection.connect((err) => {
+    if (err) {
+        console.log("Error connecting to the database: ", err);
+    } else {
+        console.log("Connected to the database!");
+    }
+});
+
+// Serve static files from a public directory
+app.use(express.static("home"));
+
+
+
+
+
+
+
+// Start a server
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});

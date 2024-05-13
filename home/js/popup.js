7fc8d8c0de96f089fb1ae2d148941340fd7c6666
sttadic <stjepan.tadich@gmail.com>
2024-05-13 01:29:03 +0100

    Login popup style and javascri to trigger popup and blur content around it implemented, and added to main templates. Popup background image added

---
+++ home/js/popup.js
@@ -0,0 +1,15 @@
+// Function that triggers login popup
+function togglePopup() {
+    let username = document.getElementById("username");
+    let blurAll = document.querySelectorAll(".blur");
+    let popup = document.getElementById("popup");
+    // Togle classes for all .blur and .popup elements to add/remove blur and make popup appear/disapear
+    blurAll.forEach(element => {
+        element.classList.toggle("blur-active");
+    });
+    popup.classList.toggle("popup-active");
+    // Focus on username and clear input fields every time popup is reopened
+    username.focus();
+    username.value="";
+    document.getElementById("password").value = "";
+};  

59ebc50df30f65d832c355a3889365c0c4b47a03
sttadic <stjepan.tadich@gmail.com>
2024-05-14 13:14:11 +0100

    Changed login form submission to open the current page after login in authenticated rather than been always redirected to the home page. Other parts of this block of code are changed for submission to perform better

--- home/js/popup.js
+++ home/js/popup.js
@@ -16,28 +16,34 @@ function togglePopup() {
 };  
 
 
-// AJAX login form submission
-// Listen for the click event
+// AJAX login popup form submission
+// Listen for the click event on submit button
 document.getElementById("login-submit").addEventListener("click", function(event) {
     // Prevent default form submission
     event.preventDefault();
 // Instantiate XMLHttpRequest object
 let xhttp = new XMLHttpRequest();
 xhttp.open("POST", "/login", true);
-// Set the content type to JSON
+// Set request header content type to JSON
 xhttp.setRequestHeader("Content-Type", "application/json");
 xhttp.onreadystatechange = function() {
-    // If server responds with status 401
-    if (xhttp.readyState === 4 && xhttp.status === 401) {
-        // Parse JSON
-        let data = JSON.parse(xhttp.responseText);
-        // Check for error message and display it within a login popup
-        if (data && data.error) {
-            document.getElementById("login-error").innerHTML = "Invalid username/password";
+    // Status of request is 4 (response is ready)
+    if (xhttp.readyState === 4) {
+        // Server responds with status 401 (unauthorized)
+        if (xhttp.status === 401) {
+            // Parse JSON
+            let data = JSON.parse(xhttp.responseText);
+            // Check for error message and display it within a login popup
+            if (data && data.error) {
+                document.getElementById("login-error").innerHTML = "Invalid username/password";
+            }
+        // Server responds with 200 (all ok) - open the page from which login was called
+         } else if (xhttp.status === 200) {
+            let activePage = document.querySelector(".active").getAttribute("href");
+            window.location.href = activePage;
+        } else {
+            alert("Ups, something went wrong! Status code: " + xhttp.status);
         }
-      // If all ok
-    } else if (xhttp.readyState === 4 && xhttp.status === 200) {
-        window.location.href = "/";
     }
 }
 // Send login data as JSON to the server
